6440ad8b536f5067669e7a1ff2fdc4712cba0242
Godgift <dr.gggsquare@gmail.com>
2022-10-20 11:11:40 +0100

    *mod: use productAPI

--- src/api/productAPI.js
+++ src/api/productAPI.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const productAPI = async () => {
     const data = await axios.get('https://fh-api-dev.herokuapp.com/api/products-service/products/website/CAD?page=0&limit=8');
-    // console.log(data.data.data.products)
     let newData = data.data.data.products
     return newData;
 };
--- src/components/products.js
+++ src/components/products.js
@@ -1,24 +1,17 @@
 import React, { useState } from "react";
-import axios from "axios";
-// import { productAPI } from '../api/productAPI';
+import { productAPI } from '../api/productAPI';
 import ProductCard from "./product-card";
 
-const client = axios.create({ 
-    baseURL: 'https://fh-api-dev.herokuapp.com/api/products-service/products/website/CAD?page=0&limit=8'
-  });
 
 function Products () {
     const [products, setProducts] = useState([]);
     React.useEffect(() => {
         async function getProducts() {
-            await client.get().then((response) => {
-                setProducts(response.data.data.products);
-            }).catch((err) => {
-                console.log(err);
-            });              
+            let data = await productAPI(); 
+            setProducts(data)           
         }
         getProducts();
-        }, [])
+    }, [])
 
     return (
         <div className="margin2 Value-box">
